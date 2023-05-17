b8115218b0782c094011ea7eba51c9a96ebbcb77
VadymWebC <vadymcdevelweb@gmail.com>
2023-05-17 23:41:54 +0300

    upd handler

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,5 +1,21 @@
+import path from 'path'
+import fs from 'fs'
+
+function buildPath() {
+    return path.join(process.cwd, 'data', 'data.json')
+}
+
+function extractData(filePath) {
+    const jsonData = fs.readFileSync(filePath)
+    return JSON.parse(jsonData)
+}
+
 export default function handler(req, res) {
     const { method } = req
+
+    const filePath = buildPath()
+    const { events_categories, allEvents } = extractData(filePath)
+
     if (method === 'POST') {
         const { email, eventId } = req.body
         res.status(200).json({
