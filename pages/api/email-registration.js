eb8476668a45bbd73a0e986b528dde9037bde9b9
VadymWebC <vadymcdevelweb@gmail.com>
2023-05-18 11:21:37 +0300

    email sing up implementation

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -2,7 +2,7 @@ import path from 'path'
 import fs from 'fs'
 
 function buildPath() {
-    return path.join(process.cwd, 'data', 'data.json')
+    return path.join(process.cwd(), 'data', 'data.json')
 }
 
 function extractData(filePath) {
@@ -16,8 +16,34 @@ export default function handler(req, res) {
     const filePath = buildPath()
     const { events_categories, allEvents } = extractData(filePath)
 
+    if (!allEvents) {
+        return res.status(404).json({
+            message: 'Events data not found',
+        })
+    }
     if (method === 'POST') {
         const { email, eventId } = req.body
+        const newAllEvents = allEvents.map((ev) => {
+            if (ev.id === eventId) {
+                if (ev.emails_registered.includes(email)) {
+                    res.status(409).json({
+                        message: 'This email has already been registered',
+                    })
+                    return ev
+                }
+                return {
+                    ...ev,
+                    emails_registered: [...ev.emails_registered, email],
+                }
+            }
+            return ev
+        })
+
+        fs.writeFileSync(
+            filePath,
+            JSON.stringify({ events_categories, allEvents: newAllEvents })
+        )
+
         res.status(200).json({
             message: `You has been registred successfully with the email: ${email} ${eventId}`,
         })
